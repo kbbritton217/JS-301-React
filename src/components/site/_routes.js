c2586e97ec8953f6f69d6a99f619f0cff29936d7
Kaylea Britton <kbbritton217@gmail.com>
2018-03-12 15:25:46 -0400

    components

---
+++ src/components/site/_routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Home from './Home';
+import FunctionalComponentDemo from '../concepts/FunctionalComponentDemo'
+import Resources from './Resources';
+
+export const routes = [
+    {
+        path: '/' || '/home',
+        exact: true,
+        main: () => <Home />
+    },
+    {
+        path: '/functionalcomponent',
+        exact: true,
+        main: () => < FunctionalComponentDemo />
+    },
+    {
+        path: '/resources',
+        exact: true,
+        main: () => <Resources />
+    },
+]
